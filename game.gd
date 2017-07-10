5a2a053782ab509ad3fe6d31578240d22945585a
Axel Lopez <csraxll@gmail.com>
2017-07-10 04:51:14 -0300

    Mejora de maquina de estados de la escena game.tscn

--- game.gd
+++ game.gd
@@ -35,38 +35,41 @@ func _ready():
 	# Initialization here
 	
 func _on_TextureFrame_input_event( ev , celda):
-	var winner= PLAYER_NONE
+	var j
 	if (game_mode == GAME_MODE_1PLAYER):
 		if (game_state == GAME_STATE_P1):
-			if (ev.type==InputEvent.MOUSE_BUTTON and ev.pressed and logic_cells[celda.x][celda.y] == PLAYER_NONE and game_state != GAME_STATE_FINISHED):
+			if (ev.type==InputEvent.MOUSE_BUTTON and ev.pressed and logic_cells[celda.x][celda.y] == PLAYER_NONE):
+				logic_cells[celda.x][celda.y]= game_state
 				celda.check(game_state)
 				check_count+=1
-				logic_cells[celda.x][celda.y]= game_state
-				game_state=  (game_state+1)%2
-				_update_player_label(game_state)
-				winner= _is_there_a_game()
-				if (winner == PLAYER_NONE and check_count< 9):
-		#else: #IA plays...
+				if (_is_there_a_game(PLAYER_1) or check_count == 9):
+					game_state= GAME_STATE_FINISHED
+				else:
+					game_state= GAME_STATE_P2
+					_update_player_label(game_state)
+				if (game_state == GAME_STATE_P2):
+					get_node("Timer").start()
+					yield(get_node("Timer"), "timeout")
 					_ia()
 					check_count+=1
-					#for x in range(3):
-					#	print("%d,%d,%d" % logic_cells[x])
-					#print("------")
-					game_state= (game_state+1)%2
-					_update_player_label(game_state)
-					winner= _is_there_a_game()
-	if (winner != PLAYER_NONE):
-		get_node("WhoPlaysControl/NowPlaysLabel").hide()
-		get_node("WhoPlaysControl/WinLabel").show()
-		get_node("WhoPlaysControl/RestartButtonLabel").show()
-		game_state= GAME_STATE_FINISHED
-		_update_player_label(winner)
-	if (check_count>=9 and winner == PLAYER_NONE):
-		game_state= GAME_STATE_FINISHED
-		get_node("WhoPlaysControl/TieLabel").show()
-		get_node("WhoPlaysControl/PlayerLabel").hide()
-		get_node("WhoPlaysControl/NowPlaysLabel").hide()
-		get_node("WhoPlaysControl/RestartButtonLabel").show()
+					if (_is_there_a_game(PLAYER_2) or check_count == 9):
+						game_state= GAME_STATE_FINISHED
+					else:
+						game_state= GAME_STATE_P1
+						_update_player_label(game_state)
+						yield(playerLabel, "draw")
+				if (game_state== GAME_STATE_FINISHED):
+					get_node("WhoPlaysControl/NowPlaysLabel").hide()
+					get_node("WhoPlaysControl/RestartButtonLabel").show()
+					if (_is_there_a_game(PLAYER_1) or _is_there_a_game(PLAYER_2)):
+						get_node("WhoPlaysControl/WinLabel").show()
+						get_node("WhoPlaysControl/TieLabel").hide()
+					else:
+						_update_player_label(game_state)
+						#yield(playerLabel, "draw")
+						get_node("WhoPlaysControl/WinLabel").hide()
+						get_node("WhoPlaysControl/TieLabel").show()
+				
 	#print(check_count)
 		
 func _on_TextureFrame_mouse_enter(celda):
@@ -87,163 +90,121 @@ func _restart():
 	get_node("WhoPlaysControl/WinLabel").hide()
 	get_node("WhoPlaysControl/TieLabel").hide()
 	get_node("WhoPlaysControl/RestartButtonLabel").hide()
+	get_node("WhoPlaysControl/PlayerLabel").show()
 	for celda in celdas:
 		celda.check(PLAYER_NONE)
 	for x in range(3):
 		for y in range(3):
 			logic_cells[x][y]=PLAYER_NONE
 
-# Hacer:
-#	Aumentar prioridad de esquinas donde se pueden hacer jugadas 
-#	Tener dos modos: Medio - Elige primer movimiento fuera de una esquina - Dificil - Elige primer movimiento en el medio o en una esquina
-#
 func _ia():
-	if (step == 0 and logic_cells[1][1] == PLAYER_NONE):
-		logic_cells[1][1] = game_state
-		for celda in celdas:
-			if (celda.x== 1 and celda.y== 1):
-				celda.check(game_state)
-		step+=1
-	else:
-		for x in range(3):
-			for y in range(3):
-				ia_cells[x][y]=0
-		for x in range(3):
-			for y in range(3):
-				if (logic_cells[x][y]==GAME_STATE_P1):
-					_ia_check_ia_cell(x+1,y)
-					_ia_check_ia_cell(x,y+1)
-					_ia_check_ia_cell(x-1,y)
-					_ia_check_ia_cell(x,y-1)
-					if (step!=1):
-						_ia_check_ia_cell(x+2,y)
-						_ia_check_ia_cell(x,y+2)
-						_ia_check_ia_cell(x-2,y)
-						_ia_check_ia_cell(x,y-2)
-					if (_ia_cell_middle_or_corner(x,y)):
-						for i in range (2):
-							_ia_check_ia_cell(x+1,y+1)
-							_ia_check_ia_cell(x-1,y+1)
-							_ia_check_ia_cell(x-1,y-1)
-							_ia_check_ia_cell(x+1,y-1)
-							_ia_check_ia_cell(x+2,y+2)
-							_ia_check_ia_cell(x-2,y+2)
-							_ia_check_ia_cell(x-2,y-2)
-							_ia_check_ia_cell(x+2,y-2)
-		_ia_possible_games()
-		_ia_check_cell()
+	var max_value= -9999
+	var x_selected=1
+	var y_selected=1
+	var value
 	for x in range(3):
-		print("%d,%d,%d" % ia_cells[x])
-	print("------")
-	
+		for y in range(3):
+			if (_ia_is_cell_empty(x,y)):
+				_ia_check_ia_cell(x,y,PLAYER_2)
+				value= _ia_minimize_game()
+				if (value >= max_value):
+					max_value = value
+					x_selected= x
+					y_selected= y
+				_ia_check_ia_cell(x,y,PLAYER_NONE)
+	_ia_check_cell(x_selected,y_selected)
+	#print("x_selected %d, y_selected %d" % [x_selected,y_selected])
+#	for x in range(3):
+#		print("%d,%d,%d" % logic_cells[x])
+#	print("------")
 
-func _ia_check_ia_cell(x,y):
+func _ia_minimize_game():
+	if (_is_there_a_game(PLAYER_2)):
+		return 1
+	if (_ia_are_all_cells_checked()):
+		return 0
+	var min_value= 9999
+	var value
+	for x in range(3):
+		for y in range(3):
+			if (_ia_is_cell_empty(x,y)):
+				_ia_check_ia_cell(x,y,PLAYER_1)
+				value = _ia_maximize_game()
+				if (value < min_value):
+					min_value = value
+				_ia_check_ia_cell(x,y,PLAYER_NONE)
+	return value
+		
+		
+func _ia_maximize_game():
+	if (_is_there_a_game(PLAYER_1)):
+		return -1
+	if (_ia_are_all_cells_checked()):
+		return 0
+	var max_value= -9999
+	var value
+	for x in range(3):
+		for y in range(3):
+			if (_ia_is_cell_empty(x,y)):
+				_ia_check_ia_cell(x,y,PLAYER_2)
+				value = _ia_minimize_game()
+				if (value > max_value):
+					max_value = value
+				_ia_check_ia_cell(x,y,PLAYER_NONE)
+	return value
+
+func _ia_are_all_cells_checked():
+	for x in range(3):
+		for y in range(3):
+			if (logic_cells[x][y] == PLAYER_NONE):
+				return false
+	return true
+
+func _ia_is_cell_empty(x,y):
+	if (logic_cells[x][y] == PLAYER_NONE):
+		return true
+	return false
+
+func _ia_check_ia_cell(x,y,player):
 	if (x>=0 and x <= 2 and y>=0 and y <= 2):
-		if (logic_cells[x][y] == PLAYER_1 or logic_cells[x][y] == PLAYER_2):
-			ia_cells[x][y]=0
-		else:
-			ia_cells[x][y]+=1
+		logic_cells[x][y]= player
 
 func _ia_cell_middle_or_corner(x,y):
 	if ((x==2 and y==0) or (x==0 and y==2) or (x==y)):
 		return true
 	return false
 
-func _ia_possible_games():
-	for x in range(3):
-		for y in range (3):
-			if (logic_cells[x][y] != PLAYER_NONE and logic_cells[x][(y+1)%3] != PLAYER_NONE):
-				if (logic_cells[x][y] == logic_cells[x][(y+1)%3]):
-					_ia_check_ia_cell(x,(y+2)%3)
-					if (logic_cells[x][y]== PLAYER_2):
-						_ia_check_ia_cell(x,(y+2)%3)
-				else:
-					ia_cells[x][(y+2)%3]-=1
-					break
-	for y in range(3):
-		for x in range(3):
-			if (logic_cells[x][y] != PLAYER_NONE and logic_cells[(x+1)%3][y] != PLAYER_NONE):
-				if (logic_cells[x][y] == logic_cells[(x+1)%3][y]):
-					_ia_check_ia_cell((x+2)%3,y)
-					if (logic_cells[x][y]== PLAYER_2):
-						_ia_check_ia_cell((x+2)%3,y)
-				else:
-					ia_cells[(x+2)%3][y]-=1
-					break
-	for i in range(3):
-		if (logic_cells[i][i] != PLAYER_NONE and logic_cells[(i+1)%3][(i+1)%3] != PLAYER_NONE):
-			if (logic_cells[i][i] == logic_cells[(i+1)%3][(i+1)%3]):
-				_ia_check_ia_cell((i+2)%3,(i+2)%3)
-				if (logic_cells[i][i]== PLAYER_2):
-					_ia_check_ia_cell((i+2)%3,(i+2)%3)
-			else:
-				ia_cells[(i+2)%3][(i+2)%3]-=1
-				break
-	if (logic_cells[2][0] != PLAYER_NONE and logic_cells[1][1] != PLAYER_NONE):
-		if (logic_cells[2][0] == logic_cells[1][1]):
-			_ia_check_ia_cell(0,2)
-			if (logic_cells[2][0] == PLAYER_2):
-				_ia_check_ia_cell(0,2)
-		else:
-			ia_cells[0][2]-=1
-	
-	if (logic_cells[1][1] != PLAYER_NONE and logic_cells[0][2] != PLAYER_NONE):
-		if (logic_cells[1][1] == logic_cells[0][2]):
-			_ia_check_ia_cell(2,0)
-			if (logic_cells[1][1] == PLAYER_2):
-				_ia_check_ia_cell(2,0)
-		else:
-			ia_cells[2][0]-=1
-			
-	if (logic_cells[0][2] != PLAYER_NONE and logic_cells[2][0] != PLAYER_NONE):
-		if (logic_cells[0][2] == logic_cells[2][0]):
-			_ia_check_ia_cell(1,1)
-			if (logic_cells[0][2] == PLAYER_2):
-				_ia_check_ia_cell(1,1)
-		else:
-			ia_cells[1][1]-=1
-
-func _ia_check_cell():
-	var best_options = []
-	var index
-	var max_priority= 0
-	for x in range(3):
-		for y in range (3):
-			if (ia_cells[x][y] == max_priority):
-				best_options.append(Vector2(x,y))
-			elif(ia_cells[x][y] > max_priority):
-				best_options.clear()
-				best_options.append(Vector2(x,y))
-				max_priority= ia_cells[x][y]
-	randomize()
-	var i=randi() % best_options.size()
-	logic_cells[best_options[i].x][best_options[i].y] = game_state
+func _ia_check_cell(x,y):
+	logic_cells[x][y] = PLAYER_2
 	for celda in celdas:
-		if (celda.x== best_options[i].x and celda.y== best_options[i].y):
-			celda.check(game_state)
+		if (celda.x== x and celda.y== y):
+			celda.check(PLAYER_2)
 	
 				
-func _is_there_a_game():
-	for x in range(3):
-		if (logic_cells[x][0] !=PLAYER_NONE and logic_cells[x][0] == logic_cells[x][1] and logic_cells[x][1] == logic_cells[x][2] ):
-			return logic_cells[x][0]
-	for y in range(3):
-		if (logic_cells[0][y] !=PLAYER_NONE and logic_cells[0][y] == logic_cells[1][y] and logic_cells[1][y] == logic_cells[2][y] ):
-			return logic_cells[0][y]
-	if (logic_cells[1][1] != PLAYER_NONE):
-		if (logic_cells[0][0] == logic_cells[1][1] and logic_cells[1][1] == logic_cells[2][2]):
-			return logic_cells[1][1]
-		if (logic_cells[2][0] == logic_cells[1][1] and logic_cells[1][1] == logic_cells[0][2]):
-			return logic_cells[1][1]
-	return PLAYER_NONE
+func _is_there_a_game(player):
+	var boolean = (logic_cells[0][0] == player and logic_cells[0][1] == player and logic_cells[0][2] == player )
+	boolean = boolean || (logic_cells[1][0] == player and logic_cells[1][1] == player and logic_cells[1][2] == player )
+	boolean = boolean || (logic_cells[2][0] == player and logic_cells[2][1] == player and logic_cells[2][2] == player )
+	
+	boolean = boolean || (logic_cells[0][0] == player and logic_cells[1][0] == player and logic_cells[2][0] == player )
+	boolean = boolean || (logic_cells[0][1] == player and logic_cells[1][1] == player and logic_cells[2][1] == player )
+	boolean = boolean || (logic_cells[0][2] == player and logic_cells[1][2] == player and logic_cells[2][2] == player )
+	
+	boolean = boolean || (logic_cells[0][0] == player and logic_cells[1][1] == player and logic_cells[2][2] == player )
+	boolean = boolean || (logic_cells[2][0] == player and logic_cells[1][1] == player and logic_cells[0][2] == player )
+	return boolean
 	
 func _update_player_label(player):
 	if (player == GAME_STATE_P1):
 		playerLabel.set_text("Jugador 1")
 		playerLabel.add_color_override("font_color", Color(0.867188,0.165985,0.165985))
-	else:
+		playerLabel.update()
+	elif (player == GAME_STATE_P2):
 		playerLabel.set_text("Jugador 2")
 		playerLabel.add_color_override("font_color", Color(0.270676,0.631152,0.949219))
+		playerLabel.update()
+	else:
+		playerLabel.hide()
 
 func _on_RestartButtonLabel_input_event( ev ):
 	if (ev.type==InputEvent.MOUSE_BUTTON and ev.pressed):
